1b8d8772c9bed2db58fdf17a7055484918ade7a8
akbararyo <akbararyo@gmail.com>
2019-05-21 12:18:37 +0700

    first commit

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Speak();
+
+            Cat cat = new Cat();
+            animal = cat;
+            //cat.Speak();
+            animal.Speak();
+
+            Chicken chicken = new Chicken();
+            chicken.Speak();
+
+            Dog dog = new Dog();
+            dog.Speak();
+
+            Console.ReadKey();
+        }
+
+    }
+    
+}
